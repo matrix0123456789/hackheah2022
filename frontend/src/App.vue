0fd978f5de4b36cb517b60488317a2ae385545fb
DESKTOP-B0B8NRK\pawel <pawelle20@gmail.com>
2022-11-20 05:47:28 +0100

    Style login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
-    <ConnectionError v-if="websocketStore.connectionError"/>
+    <ConnectionError v-if="websocketStore.connectionError" />
     <div v-else>
-      <Connecting v-if="websocketStore.connecting"/>
+      <Connecting v-if="websocketStore.connecting" />
       <div v-else-if="websocketStore.connected">
-        <AppContent/>
+        <AppContent />
       </div>
       <div v-else>
-        <Login @onLogin="websocketStore.connect"/>
+        <Login @onLogin="websocketStore.connect" />
       </div>
     </div>
   </main>
@@ -16,7 +16,7 @@
 <script>
 import Connecting from "./components/Connecting.vue";
 import ConnectionError from "@/components/ConnectionError.vue";
-import {useWebsocketStore} from '@/stores/websocketStore'
+import { useWebsocketStore } from '@/stores/websocketStore'
 
 import AppContent from "@/components/AppContent.vue";
 import Login from "@/components/Login.vue";
@@ -50,7 +50,32 @@ header {
   line-height: 1.5;
 }
 
+
 body {
   margin: 0;
+  font-family: 'Dancing Script', cursive;
+  font-family: 'Nanum Gothic', sans-serif;
+
+  button {
+      background: rgb(106, 59, 8);
+      background: linear-gradient(0deg, rgba(106, 59, 8, 1) 9%, rgba(219, 104, 36, 1) 100%);
+      border-radius: 7px;
+      border: 4px solid rgba(106, 59, 8, 1);
+      border-top: none;
+      border-bottom: 5px solid rgba(106, 59, 8, 1);
+      padding: 5px 10px;
+      font-size: 17px;
+      font-weight: 700;
+      color: white;
+      transition: 200ms;
+      cursor: pointer;
+
+      &.disabled {
+        background: gray;
+        border: 4px solid gray;
+        border-bottom: 5px solid gray;
+        opacity: 0.7;
+      }
+    }
 }
 </style>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,15 +1,17 @@
 <template>
-<div class="login-widget">
-  <div>Zaloguj sie</div>
-  <label for="login">Login</label>
-  <input name="login" v-model="username">
-  <button @click="login">Zaloguj</button>
-</div>
+  <div class="login-widget">
+    <div class="wrapper">
+      <h5>Logowanie</h5>
+      <label for="login">Nazwa użytkownika</label>
+      <input name="login" v-model="username">
+      <button @click="login">Zaloguj</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import {useWebsocketStore} from "@/stores/websocketStore";
-import {useNavigatorStore} from "@/stores/navigator";
+import { useWebsocketStore } from "@/stores/websocketStore";
+import { useNavigatorStore } from "@/stores/navigator";
 
 export default {
   name: "Login",
@@ -20,21 +22,94 @@ export default {
     }
   },
   methods: {
-    login(){
+    login() {
       this.$emit("onLogin", this.username);
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .login-widget {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  width: 100vw ;
-  font-size: 40px   ;
+  width: 100vw;
+  font-size: 40px;
+  position: relative;
+
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("../assets/bg.png");
+    background-repeat: no-repeat, repeat;
+    background-size: cover;
+    /* Resize the background image to cover the entire container */
+    filter: grayscale(100%);
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    align-items: center;
+    padding: 30px;
+    border-radius: 5px;
+    border: 3px solid #c1c1c1;
+    background: #121212;
+
+    h5 {
+      margin-top: 0;
+      font-size: 20px;
+      color: white;
+    }
+
+    label {
+      font-size: 16px;
+      margin-bottom: 5px;
+      color: white;
+    }
+
+    input {
+      background: rgb(106, 59, 8);
+      border-radius: 7px;
+      border: 4px solid rgba(106, 59, 8, 1);
+      padding: 5px 10px;
+      font-size: 17px;
+      font-weight: 700;
+      color: white;
+      transition: 200ms;
+      // cursor: pointer;
+      margin-bottom: 30px;
+      background: linear-gradient(0deg, rgba(106, 59, 8, 0.8) 9%, rgba(219, 104, 36, 0.8) 100%);
+    }
+
+    button {
+      background: rgb(106, 59, 8);
+      background: linear-gradient(0deg, rgba(106, 59, 8, 1) 9%, rgba(219, 104, 36, 1) 100%);
+      border-radius: 7px;
+      border: 4px solid rgba(106, 59, 8, 1);
+      border-top: none;
+      border-bottom: 5px solid rgba(106, 59, 8, 1);
+      padding: 5px 10px;
+      font-size: 17px;
+      font-weight: 700;
+      color: white;
+      transition: 200ms;
+      cursor: pointer;
+    }
+
+    button:hover {
+      opacity: 0.9;
+    }
+  }
+
 }
 </style>
